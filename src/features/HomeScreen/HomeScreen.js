d028b9324e131607a824b49a63738a3ec5f68b69
Pratik Bhumkar <pratik.bhumkar@mydna.life>
2021-02-27 23:44:18 +1100

    Adding loading image, removing warnings.

--- src/features/HomeScreen/HomeScreen.js
+++ src/features/HomeScreen/HomeScreen.js
@@ -3,19 +3,30 @@ import { getMovies } from "../../redux/selectors";
 import { connect } from "react-redux";
 import MovieContainer from './components/MovieContainer'
 import MovieSearchBox from './components/MovieSearchBox'
+import loading from "../../assets/loading.gif";
 const HomeScreen = ({ movies }) => {
-    return (
-        <div className="container">
-            <MovieSearchBox movies={movies}/>
-            <div class="grid-container">
-                {
-                    movies?.map((movie, index)=>{
-                        return <MovieContainer movie={movie}/>
-                    })
-                }
+    if (movies) {
+        return (
+            <div className="container">
+                <MovieSearchBox movies={movies} />
+                <div className="grid-container">
+                    {
+                        movies?.map((movie, index) => {
+                            return <MovieContainer movie={movie} key={index} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container">
+                <img src={loading} />
+                <h1>Please wait while the movies are loaded.</h1>
+            </div>
+        )
+    }
+
 }
 
 const mapStateToProps = state => {
